ed3e694b529994791d3e5556d84f617bb275bac2
Pradeep-Deep14 <123350286+Pradeep-Deep14@users.noreply.github.com>
2024-02-16 20:19:58 +0530

    first commit

---
+++ 7.py
@@ -0,0 +1,7 @@
+dict_x = {"name": "Prakash", "Age": 23}
+dict_y = {"name": "Joy"}
+
+dict_x.update(dict_y)  # This will update dict_x with the key-value pairs from dict_y
+update_val = dict_x
+
+print(update_val)
